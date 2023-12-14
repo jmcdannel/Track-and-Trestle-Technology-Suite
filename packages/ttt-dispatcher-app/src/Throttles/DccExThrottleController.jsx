acd03008fad96d757e6d2ef3e1f31f18fb782add
Josh McDannel <mcdannel@gmail.com>
2023-12-14 08:28:46 -0700

    fix rear facing consist issue

--- packages/ttt-dispatcher-app/src/Throttles/DccExThrottleController.jsx
+++ packages/ttt-dispatcher-app/src/Throttles/DccExThrottleController.jsx
@@ -3,6 +3,8 @@ import { Context } from '../Store/Store';
 import { usePrevious } from '../Shared/Hooks/usePrevious';
 import dccApi from '../Shared/api/dccApi';
 
+const SWITCH_DIR_DELAY = 250;
+
 export const DccExThrottleController = props => {
 
     const { speed, address, consist } = props;
@@ -11,6 +13,16 @@ export const DccExThrottleController = props => {
     const prevSpeed = usePrevious(speed);
 
     useEffect(async () => {
+      const setConsist = async () => {
+        consist.forEach(async (consistAddress) => {
+          await dccApi.setSpeed(Math.abs(consistAddress), consistAddress > 0 ? speed : -speed);
+        });
+      }
+      const stopConsist = async () => {
+        consist.forEach(async (consistAddress) => {
+          await dccApi.setSpeed(Math.abs(consistAddress), 0);
+        });
+      }
       if (!address) {
         // TODO: handle error
         return;
@@ -25,21 +37,19 @@ export const DccExThrottleController = props => {
         //change direction to forward
         console.log('[DccExThrottleController] change direction to forward');
         await dccApi.setSpeed(address, 0); // stop
+        consist && stopConsist(consist);
         delay = SWITCH_DIR_DELAY;
       } else if (speed < 0 && prevSpeed > 0) {
         //change direction to reverse
         console.log('[DccExThrottleController] change direction to reverse');
         await dccApi.setSpeed(address, 0); // stop
+        consist && stopConsist(consist);
         delay = SWITCH_DIR_DELAY;
       }
       setTimeout(async () => {
         console.log('[DccExThrottleController] sendLocoSpeed', speed, consist);
         await dccApi.setSpeed(address, speed);
-        if (consist) {
-          consist.forEach(async (consistAddress) => {
-            await dccApi.setSpeed(Math.abs(consistAddress), speed);
-          });
-        }
+        consist && setConsist(consist);
         await dispatch({ type: 'UPDATE_LOCO', payload: { address, speed } });
 
       }, delay);
