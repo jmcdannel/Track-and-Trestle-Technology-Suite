07020b45d3330e26845e4ec5292c2a93a2fd29bc
Josh McDannel <mcdannel@gmail.com>
2024-02-06 15:26:56 -0700

    Update turnout styles and add turnout state classes

--- packages/ttt-dispatcher-app/src/Routes/RouteMap.jsx
+++ packages/ttt-dispatcher-app/src/Routes/RouteMap.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { useConnectionStore } from '../Store/useConnectionStore';
+import { useTurnoutStore } from '../Store/useTurnoutStore';
 import useLayoutRoute from '../Routes/useLayoutRoute';
 import TamMapSvg from './tamarack-junction-map.svg?react';
 import BetatrackMapSvg from './betatrack-map.svg?react';
@@ -10,19 +11,30 @@ import './RouteMap.scss';
 const RouteMap = props => {
 
   const layoutId = useConnectionStore(state => state.layoutId);
+  const turnouts = useTurnoutStore(state => state.turnouts);
   const {
     handleMapClick,
     computedRoutes
   } = useLayoutRoute();
 
-  const mapClassName = computedRoutes()?.reduce((acc, rte) => {
+  const routeClasses = computedRoutes()?.reduce((acc, rte) => {
     return `${acc} ${rte.disabled ? `disabled-${rte.svgId}` : ''}`;
   }, '' )
+  const turnoutClasses = turnouts.map(t => {
+    // return t.turnoutId
+    return t.state
+      ? `turnout-${t.turnoutId}-straight`
+      : `turnout-${t.turnoutId}-divergent`
+  }).join(' ')
+    
+  const mapClassName = `${routeClasses} ${turnoutClasses}`
+
+  console.log('[RouteMap] mapClassName', mapClassName)
 
   function getMap() {
     switch (layoutId) {
       case 'tam':
-        return <TamMapSvg onClick={handleMapClick} />
+        return <TamMapSvg className={mapClassName} onClick={handleMapClick} />
       case 'betatrack':
         return <BetatrackMapSvg className={mapClassName} onClick={handleMapClick} />
       default:
