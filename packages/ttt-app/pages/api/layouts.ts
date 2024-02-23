a62416d7e91a6be0e1db95cca8fc43c3a41e9cd6
Josh McDannel <mcdannel@gmail.com>
2024-02-23 08:39:54 -0700

    Refactor API handler and add CORS middleware

--- packages/ttt-app/pages/api/layouts.ts
+++ packages/ttt-app/pages/api/layouts.ts
@@ -1,17 +1,44 @@
+import Cors from 'cors'
 import clientPromise from "../../lib/mongodb";
 
-export default async (req:any, res:any) => {
-   try {
-       const client = await clientPromise;
-       const db = client.db('trestledb');
 
-       const layouts = await db
-           .collection('layouts')
-           .find({})
-           .toArray();
+// Initializing the cors middleware
+const cors = Cors({
+methods: ['GET', 'HEAD'],
+})
 
-       res.json(layouts);
-   } catch (e) {
-       console.error(e);
-   }
-};
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+        if (result instanceof Error) {
+            return reject(result)
+        }
+
+        return resolve(result)
+        })
+    })
+}
+
+async function handler(req, res) {
+    // Run the middleware
+    await runMiddleware(req, res, cors)
+  
+    // Rest of the API logic
+    try {
+        const client = await clientPromise;
+        const db = client.db('trestledb');
+ 
+        const layouts = await db
+            .collection('layouts')
+            .find({})
+            .toArray();
+ 
+        res.json(layouts);
+    } catch (e) {
+        console.error(e);
+    }
+  }
+  
+export default handler
