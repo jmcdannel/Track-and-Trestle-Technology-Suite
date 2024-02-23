b9f1dee69481c793cc8a335f0e769ac2804bb5d1
Josh McDannel <mcdannel@gmail.com>
2024-02-23 08:13:15 -0700

    Add CORS headers to API routes

--- packages/ttt-app/next.config.mjs
+++ packages/ttt-app/next.config.mjs
@@ -1,4 +1,7 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   async headers() {
     return [
       {
@@ -13,4 +16,6 @@ module.exports = {
       }
     ]
   }
-};
+}
+ 
+export default nextConfig
