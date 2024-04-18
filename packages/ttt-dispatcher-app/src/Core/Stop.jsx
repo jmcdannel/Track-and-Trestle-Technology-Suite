33f3bf4d2e98b5726b02683528ac190d311de550
Josh McDannel <mcdannel@gmail.com>
2024-04-18 11:32:26 -0600

    Refactor API routes for turnouts and effects to use dynamic parameters instead of fixed layoutId and effectId in [...params].ts

--- packages/ttt-dispatcher-app/src/Core/Stop.jsx
+++ packages/ttt-dispatcher-app/src/Core/Stop.jsx
@@ -2,18 +2,20 @@ import React, { useContext } from 'react';
 import IconButton from '@mui/material/IconButton';
 import PanToolIcon from '@mui/icons-material/PanTool';
 import { useMqtt } from '../Core/Com/MqttProvider'
+import { useConnectionStore } from '../Store/useConnectionStore';
 
 const STOP = '!';
 
 export const Stop = () => {
 
+  const layoutId = useConnectionStore(state => state.layoutId);
   const { publish } = useMqtt();
 
   const handleStopClick = async () => {
 
     try {
       console.log('handleStopClick');
-      publish('ttt-dcc',  JSON.stringify({
+      publish(`@ttt/dcc/${layoutId}`,  JSON.stringify({
         action: 'dcc',
         payload: STOP
       }))
