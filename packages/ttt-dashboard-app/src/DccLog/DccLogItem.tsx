d4eff3efabb8062eeb006e8d9c7a6044f57617af
Josh McDannel <mcdannel@gmail.com>
2024-05-24 12:25:29 -0600

    Refactor DccLog and DccLogItem components to improve performance and add new features

--- packages/ttt-dashboard-app/src/DccLog/DccLogItem.tsx
+++ packages/ttt-dashboard-app/src/DccLog/DccLogItem.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "preact";
+import { FunctionComponent, JSX } from "preact";
 interface DccLogType {
   message: string,
   id: string
@@ -9,11 +9,62 @@ interface DccLogItemProps {
 }
 
 
-export const DccLogItem: FunctionComponent<DccLogItemProps> = ({ item }) => {
+export const DccLogItem: FunctionComponent<DccLogItemProps> = ({ item }): JSX.Element => {
+  const dccRegex = /<\*\s(.*?)\s\*>/;
+
+  function renderDccItem(item: DccLogType) {
+    switch (item.message.charAt(1)) {
+      case '*' : 
+        const match = item.message.match(dccRegex)
+        const formattedMessage = match ? match[1] : ''
+        return (
+          <>
+            <span className="text-red-500 bg-violet-800 m-1 rounded-full py-1 px-3 text-sm">STATUS</span>
+            <span className="text-cyan-900">{formattedMessage}</span>
+          </>
+        )
+      case 'l' : return (
+        <>
+          <span className="text-green-500 bg-blue-900 m-1 rounded-full py-1 px-3 text-sm">LOCO</span>
+          <span className="text-cyan-900">{item.message}</span>
+        </>
+        )
+      case 'H' : return (
+        <>
+          <span className="text-fuchsia-300 bg-blue-900 m-1 rounded-full py-1 px-3 text-sm">TURNOUT</span>
+          <span className="text-cyan-900">{item.message}</span>
+        </>
+      )
+      case 'Y' : return (
+        <>
+          <span className="text-yellow-500 bg-blue-900 m-1 rounded-full py-1 px-3 text-sm">OUTPUT</span>
+          <span className="text-cyan-900">{item.message}</span>
+        </>
+      )
+      default: return (
+        <>
+          <span className="text-blue-500 bg-blue-900 m-1 rounded-full py-1 px-3 text-sm">DCC</span>
+          <span className="text-cyan-900">{item.message}</span>
+        </>
+      )
+    }
+  }
+
+  function renderItem(item: DccLogType) {
+    if (item.message.startsWith('<')) { // dcc command
+      return renderDccItem(item); // Add a return statement here
+    } else {
+      return (
+        <pre>{item.message}</pre>
+      )
+    }
+  }
 
   return (
-    <pre>{item.message}</pre>
-  );
+    <pre className="p-1 mb-1 bg-violet-300 rounded-full">
+      {renderItem(item)}
+    </pre>
+  )
 
 }
 
--- packages/ttt-dashboard-app/src/Throttles/Throttles.tsx
+++ packages/ttt-dashboard-app/src/Throttles/Throttles.tsx
@@ -6,11 +6,11 @@ export const Throttles: FunctionComponent = () => {
   return (
     <>
       <button onClick={clearLog}>Clear Log</button>
-      <div className="flex flex-col-reverse max-h-80 overflow-y-auto">
-        {[...throttles.value].map(( throttle , idx) => (
-          <div key={`throttles${idx}`} className="slide-in-left">
-            <span>{throttle.address}</span> - 
-            <span>{throttle.speed}</span>
+      <div className="mt-2 flex flex-col-reverse max-h-80 overflow-y-auto">
+        {[...throttles.value].map((throttle, idx) => (
+          <div key={`throttles${idx}`} className="slide-in-left p-2 mb-1 bg-indigo-800 rounded-xl flex items-center justify-between shadow-md ">
+            <span className=" bg-cyan-950 text-white rounded-lg min-w-32 text-center px-3 py-1 ring-1 ring-slate-400 ring-offset-2 ring-offset-slate-900">{throttle.address}</span>
+            <span className="bg-gray-800 p-3 rounded-lg text-lime-400 font-bold border-solid border-white ring-1 shadow-inner shadow-slate-700">{throttle.speed}</span>
           </div>
         ))}
       </div>
