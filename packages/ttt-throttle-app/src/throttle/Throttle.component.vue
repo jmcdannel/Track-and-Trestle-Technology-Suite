716d3b6d6d016aca2ed82bfdada31521a9f055b0
Josh McDannel <mcdannel@gmail.com>
2024-06-07 10:19:55 -0600

    Refactor vue-debounce dependency to version 5.0.0

--- packages/ttt-throttle-app/src/throttle/Throttle.component.vue
+++ packages/ttt-throttle-app/src/throttle/Throttle.component.vue
@@ -50,9 +50,9 @@
     { id: 31, name: 'F31' }
   ]
 
-  const setSpeed = debounce((val: number) => currentSpeed.value = val, `${DEBOUNCE_DELAY}ms`)
+  const setSpeed = debounce((val: number): void => { currentSpeed.value = val; }, `${DEBOUNCE_DELAY}ms`)
 
-  function onSlider(e) {
+  function onSlider(e:any) {
     const newSpeed = parseInt(e.target.value);
     rangeValue.value = newSpeed;
     setSpeed(newSpeed);
@@ -231,4 +231,4 @@
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='105' viewBox='0 0 80 105'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='death-star' fill='%23000000' fill-opacity='0.4'%3E%3Cpath d='M20 10a5 5 0 0 1 10 0v50a5 5 0 0 1-10 0V10zm15 35a5 5 0 0 1 10 0v50a5 5 0 0 1-10 0V45zM20 75a5 5 0 0 1 10 0v20a5 5 0 0 1-10 0V75zm30-65a5 5 0 0 1 10 0v50a5 5 0 0 1-10 0V10zm0 65a5 5 0 0 1 10 0v20a5 5 0 0 1-10 0V75zM35 10a5 5 0 0 1 10 0v20a5 5 0 0 1-10 0V10zM5 45a5 5 0 0 1 10 0v50a5 5 0 0 1-10 0V45zm0-35a5 5 0 0 1 10 0v20a5 5 0 0 1-10 0V10zm60 35a5 5 0 0 1 10 0v50a5 5 0 0 1-10 0V45zm0-35a5 5 0 0 1 10 0v20a5 5 0 0 1-10 0V10z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
   }
 
-</style>../api/dccApi.js
+</style>
--- packages/ttt-throttle-app/src/views/FooterView.vue
+++ packages/ttt-throttle-app/src/views/FooterView.vue
@@ -1,9 +1,4 @@
 <script setup lang="ts">
-  import { storeToRefs } from 'pinia';
-  import { useConfigStore } from '../store/configStore.tsx';
-
-  const configStore = useConfigStore();
-  const { locoId } = storeToRefs(configStore);
   
 </script>
 <template>
@@ -14,18 +9,18 @@
         custom
         v-slot="{ navigate }"
       >
-        <button class="text-primary" :class="{ active: (route?.name === 'effects') }" @click="navigate" role="link">
+        <button class="text-primary" :class="{ active: ($route?.name === 'effects') }" @click="navigate" role="link"></button>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
           </svg>
         </button>
       </router-link>
       <router-link
-        :to="locoId ? `/throttle/${locoId}` : `/locos`"
+        :to="`locos`"
         custom
         v-slot="{ navigate }"
       >
-        <button class="text-primary" :class="{ active: (route?.name === 'throttle' || route?.name === 'locos') }" @click="navigate" role="link">
+        <button class="text-primary" :class="{ active: ($route?.name === 'throttle' || $route?.name === 'locos') }" @click="navigate" role="link">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
           </svg>
@@ -36,7 +31,7 @@
         custom
         v-slot="{ navigate }"
       >
-        <button class="text-primary" :class="{ active: (route?.name === 'turnouts') }" @click="navigate" role="link">
+        <button class="text-primary" :class="{ active: ($route?.name === 'turnouts') }" @click="navigate" role="link">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
           </svg>
