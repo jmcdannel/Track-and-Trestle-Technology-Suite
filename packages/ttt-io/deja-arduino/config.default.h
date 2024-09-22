bdd366ae23feb173c8460b99c5d1ec373155797e
Josh McDannel <mcdannel@gmail.com>
2024-09-22 16:25:46 -0600

    Refactor configuration files for betatrack-B and deja-arduino devices

---
+++ packages/ttt-io/deja-arduino/config.default.h
@@ -0,0 +1,17 @@
+#define DEVICE_ID "deja-arduino"
+#define ENABLE_PWM false
+#define ENABLE_OUTPUTS false
+#define ENABLE_SIGNALS false
+#define ENABLE_TURNOUTS false
+
+int OUTPINS[] = {};
+int SIGNALPINS[] = {};
+
+#define SERVOMIN 150 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 600 // This is the 'maximum' pulse length count (out of 4096)
+#define MIN_PULSE_WIDTH 650
+#define MAX_PULSE_WIDTH 2350
+#define USMIN 600     // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX 2400    // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define SERVO_COUNT 16
